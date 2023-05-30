2db3e9cd8d2d66fcaf78c9f2f909a2e63e1492bf
Robin <robin.escaffre@edu.esiee.fr>
2023-05-30 11:30:38 +0200

    Module2 Fini (style un peu)

--- src/screens/HealthGoals.js
+++ src/screens/HealthGoals.js
@@ -1,6 +1,6 @@
 import { Picker } from '@react-native-picker/picker';
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import { Button, Switch, TextInput } from 'react-native-paper';
 
 const HealthGoals = () => {
@@ -26,41 +26,55 @@ const HealthGoals = () => {
     return Math.round(BMR);
   }
   return (
-    <View>
-      <TextInput label="Age" value={age} onChangeText={setAge} keyboardType="numeric" />
-      <Text>Are you a male ?</Text>
-      <Switch
-        trackColor={{ false: '#767577', true: '#d1c4e9' }}
-        thumbColor={isMale ? '#ff4081' : '#f4f3f4'}
-        ios_backgroundColor="#3e3e3e"
-        onValueChange={toggleSwitch}
-        value={isMale}
-      />
-      <TextInput label="Height" value={height} onChangeText={setHeight} keyboardType="numeric" />
-      <TextInput label="Weight" value={weight} onChangeText={setWeight} keyboardType="numeric" />
-      <Picker
-        selectedValue={activityLevel}
-        onValueChange={(itemValue, itemIndex) => setActivityLevel(itemValue)}>
-        <Picker.Item label="Sedentary" value="Sedentary" />
-        <Picker.Item label="Light Exercise" value="Light Exercise" />
-        <Picker.Item label="Moderate Exercise" value="Moderate Exercise" />
-        <Picker.Item label="Heavy Exercise" value="Heavy Exercise" />
-        <Picker.Item label="Extra active" value="Extra active" />
-      </Picker>
-      <Picker
-        selectedValue={healthGoal}
-        onValueChange={(itemValue, itemIndex) => setHealthGoal(itemValue)}>
-        <Picker.Item label="Weight Loss" value="Weight Loss" />
-        <Picker.Item label="Weight Maintenance" value="Weight Maintenance" />
-        <Picker.Item label="Weight Gain" value="Weight Gain" />
-      </Picker>
-      <Button
-        title="calculateBMR"
-        onPress={() => setBMR(calculateBMR(age, isMale, height, weight, activityLevel, healthGoal))}
-      />
-      <Text>BMR : {BMR} calories/day</Text>
-    </View>
+    <ScrollView contentContainerStyle= {styles.container}>
+      <View>
+        <TextInput label="Age" value={age} onChangeText={setAge} keyboardType="numeric" />
+        <Text>Are you a male ?</Text>
+        <Switch
+          trackColor={{ false: '#767577', true: '#d1c4e9' }}
+          thumbColor={isMale ? '#ff4081' : '#f4f3f4'}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleSwitch}
+          value={isMale}
+        />
+        <TextInput label="Height" value={height} onChangeText={setHeight} keyboardType="numeric" />
+        <TextInput label="Weight" value={weight} onChangeText={setWeight} keyboardType="numeric" />
+        <Picker
+          selectedValue={activityLevel}
+          onValueChange={(itemValue, itemIndex) => setActivityLevel(itemValue)}>
+          <Picker.Item label="Sedentary" value="Sedentary" />
+          <Picker.Item label="Light Exercise" value="Light Exercise" />
+          <Picker.Item label="Moderate Exercise" value="Moderate Exercise" />
+          <Picker.Item label="Heavy Exercise" value="Heavy Exercise" />
+          <Picker.Item label="Extra active" value="Extra active" />
+        </Picker>
+        <Picker
+          selectedValue={healthGoal}
+          onValueChange={(itemValue, itemIndex) => setHealthGoal(itemValue)}>
+          <Picker.Item label="Weight Loss" value="Weight Loss" />
+          <Picker.Item label="Weight Maintenance" value="Weight Maintenance" />
+          <Picker.Item label="Weight Gain" value="Weight Gain" />
+        </Picker>
+        <Button
+          onPress={() => setBMR(calculateBMR(age, isMale, height, weight, activityLevel, healthGoal))}
+          buttonColor= '#2196F3'
+        >Calculate</Button>
+        <Text style={styles.titre}>BMR : {BMR} calories/day</Text>
+      </View>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+    flexGrow: 1
+  },
+  titre: {
+    fontSize: 30,
+    fontFamily: "sans-serif",
+    textAlign: "center"
+  }
+});
 
 export default HealthGoals;
