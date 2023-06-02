3bdb3c1ecf641b57ddbcf492c487ff33fd49e9d4
Tadres <nicolas.tadres@edu.esiee.fr>
2023-06-02 23:33:22 +0200

    refacto code + style Health Goals

--- src/screens/HealthGoals.js
+++ src/screens/HealthGoals.js
@@ -1,35 +1,71 @@
 import { Picker } from '@react-native-picker/picker';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import { Button, Switch, TextInput } from 'react-native-paper';
 
 const HealthGoals = () => {
+  const allActivityLevels = new Map([
+    ['Sedentary', 'Sedentary'],
+    ['Light', 'Light Exercise'],
+    ['Moderate', 'Moderate Exercise'],
+    ['Heavy', 'Heavy Exercise'],
+    ['Extra', 'Extra active'],
+  ]);
+
+  const allHealthGoals = new Map([
+    ['Loss', 'Weight Loss'],
+    ['Maintenance', 'Weight Maintenance'],
+    ['Gain', 'Weight Gain'],
+  ]);
+
   const [age, setAge] = useState('');
   const [isMale, setIsMale] = useState(false);
   const toggleSwitch = () => setIsMale((previousState) => !previousState);
   const [height, setHeight] = useState('');
   const [weight, setWeight] = useState('');
-  const [activityLevel, setActivityLevel] = useState('Sedentary');
-  const [healthGoal, setHealthGoal] = useState('Weight Loss');
+  const [activityLevel, setActivityLevel] = useState(allActivityLevels.get('Sedentary'));
+  const [healthGoal, setHealthGoal] = useState(allHealthGoals.get('Loss'));
   const [BMR, setBMR] = useState(0);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+
   function calculateBMR(age, isMale, height, weight, activityLevel, healthGoal) {
     let BMR = 0;
+
     if (isMale) BMR = 88.362 + 13.397 * weight + 4.799 * height - 5.677 * age;
     else BMR = 447.593 + 9.247 * weight + 3.098 * height - 4.33 * age;
-    if (activityLevel === 'Sedentary') BMR *= 1.2;
-    else if (activityLevel === 'Light Exercice') BMR *= 1.375;
-    else if (activityLevel === 'Moderate Exercice') BMR *= 1.55;
-    else if (activityLevel === 'Heavy Exercice') BMR *= 1.725;
-    else if (activityLevel === 'Extra active') BMR *= 1.9;
-    if (healthGoal === 'Weight Loss') BMR -= 500;
-    else if (healthGoal === 'Weight Gain') BMR += 500;
+
+    if (activityLevel === allActivityLevels.get('Sedentary')) BMR *= 1.2;
+    else if (activityLevel === allActivityLevels.get('Light')) BMR *= 1.375;
+    else if (activityLevel === allActivityLevels.get('Moderate')) BMR *= 1.55;
+    else if (activityLevel === allActivityLevels.get('Heavy')) BMR *= 1.725;
+    else if (activityLevel === allActivityLevels.get('Extra')) BMR *= 1.9;
+
+    if (healthGoal === allHealthGoals.get('Loss')) BMR -= 500;
+    else if (healthGoal === allHealthGoals.get('Gain')) BMR += 500;
+
     return Math.round(BMR);
   }
+
+  useEffect(() => {
+    if (age && height && weight) {
+      setIsButtonDisabled(false);
+    } else {
+      setIsButtonDisabled(true);
+    }
+  }, [age, height, weight]);
+
   return (
-    <ScrollView contentContainerStyle= {styles.container}>
-      <View>
-        <TextInput label="Age" value={age} onChangeText={setAge} keyboardType="numeric" />
-        <Text>Are you a male ?</Text>
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.title}>Health Goals</Text>
+      <TextInput
+        label="Age"
+        value={age}
+        onChangeText={setAge}
+        keyboardType="numeric"
+        style={styles.decalageBottom}
+      />
+      <View style={styles.switchContainer}>
+        <Text style={styles.switchLabel}>Are you a male ?</Text>
         <Switch
           trackColor={{ false: '#767577', true: '#d1c4e9' }}
           thumbColor={isMale ? '#ff4081' : '#f4f3f4'}
@@ -37,44 +73,93 @@ const HealthGoals = () => {
           onValueChange={toggleSwitch}
           value={isMale}
         />
-        <TextInput label="Height" value={height} onChangeText={setHeight} keyboardType="numeric" />
-        <TextInput label="Weight" value={weight} onChangeText={setWeight} keyboardType="numeric" />
+      </View>
+      <TextInput
+        label="Height"
+        value={height}
+        onChangeText={setHeight}
+        keyboardType="numeric"
+        style={styles.decalageBottom}
+      />
+      <TextInput
+        label="Weight"
+        value={weight}
+        onChangeText={setWeight}
+        keyboardType="numeric"
+        style={styles.decalageBottom}
+      />
+      <View style={styles.decalageBottom}>
+        <Text style={styles.pickerLabel}>Activity Level</Text>
         <Picker
           selectedValue={activityLevel}
-          onValueChange={(itemValue, itemIndex) => setActivityLevel(itemValue)}>
-          <Picker.Item label="Sedentary" value="Sedentary" />
-          <Picker.Item label="Light Exercise" value="Light Exercise" />
-          <Picker.Item label="Moderate Exercise" value="Moderate Exercise" />
-          <Picker.Item label="Heavy Exercise" value="Heavy Exercise" />
-          <Picker.Item label="Extra active" value="Extra active" />
+          onValueChange={(itemValue) => setActivityLevel(itemValue)}
+          itemStyle={styles.pickerItem}
+          numberOfLines={2}>
+          {Array.from(allActivityLevels, ([key, value]) => (
+            <Picker.Item key={value} label={value} value={value} />
+          ))}
         </Picker>
+      </View>
+      <View style={styles.decalageBottom}>
+        <Text style={styles.pickerLabel}>Health Goal</Text>
         <Picker
           selectedValue={healthGoal}
-          onValueChange={(itemValue, itemIndex) => setHealthGoal(itemValue)}>
-          <Picker.Item label="Weight Loss" value="Weight Loss" />
-          <Picker.Item label="Weight Maintenance" value="Weight Maintenance" />
-          <Picker.Item label="Weight Gain" value="Weight Gain" />
+          onValueChange={(itemValue) => setHealthGoal(itemValue)}
+          itemStyle={styles.pickerItem}>
+          {Array.from(allHealthGoals, ([key, value]) => (
+            <Picker.Item key={value} label={value} value={value} />
+          ))}
         </Picker>
-        <Button
-          onPress={() => setBMR(calculateBMR(age, isMale, height, weight, activityLevel, healthGoal))}
-          buttonColor= '#2196F3'
-        >Calculate</Button>
-        <Text style={styles.titre}>BMR : {BMR} calories/day</Text>
       </View>
+      <Button
+        onPress={() => setBMR(calculateBMR(age, isMale, height, weight, activityLevel, healthGoal))}
+        disabled={isButtonDisabled}
+        style={[styles.button, { backgroundColor: isButtonDisabled ? '#bbbbbb' : '#2196F3' }]}
+        labelStyle={styles.buttonLabel}>
+        Calculate
+      </Button>
+      <Text style={styles.title}>BMR : {BMR} calories/day</Text>
     </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flexGrow: 1,
     padding: 16,
-    flexGrow: 1
   },
-  titre: {
-    fontSize: 30,
-    fontFamily: "sans-serif",
-    textAlign: "center"
-  }
+  decalageBottom: {
+    marginBottom: 16,
+  },
+  switchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  switchLabel: {
+    marginRight: 8,
+  },
+  pickerLabel: {
+    marginBottom: 8,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  pickerItem: {
+    height: 50,
+  },
+  button: {
+    marginTop: 10,
+  },
+  buttonLabel: {
+    color: '#ffffff',
+    fontWeight: 'bold',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 10,
+  },
 });
 
 export default HealthGoals;
