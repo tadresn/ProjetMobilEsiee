c9e4b7da4b31e7b3e4e74b34055995d58f2d028e
Robin <robin.escaffre@edu.esiee.fr>
2023-06-06 16:12:13 +0200

    Module3 PresqueFini

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import AppNavigator from './src/navigation/AppNavigator';
+import { enGB, registerTranslation } from 'react-native-paper-dates'
+registerTranslation('en-GB', enGB)
 
 const App = () => {
   return <AppNavigator />;
--- src/screens/FoodDatabase.js
+++ src/screens/FoodDatabase.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, ScrollView, Image } from 'react-native';
-import { Searchbar } from 'react-native-paper';
+import { View, Text, StyleSheet, ScrollView, Image, Modal, DatePickerIOSBase } from 'react-native';
+import { Searchbar, Button } from 'react-native-paper';
 
 import { searchFoodByName } from '../services/FoodDatabaseService';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { DatePickerInput, DatePickerModal } from 'react-native-paper-dates';
 
 const FoodDatabase = () => {
   const [userSearch, setUserSearch] = useState('');
   const [resultAPI, setResultAPI] = useState(null);
+  const [currentFood, setCurrentFood] = useState(null);
+  const [selectedDate, setSelectedDate] = useState(null);
 
   async function handleSearch() {
     await searchFoodByName(userSearch)
@@ -18,7 +22,13 @@ const FoodDatabase = () => {
       });
   }
 
+  const [modalVisible, setModalVisible] = useState(false);
+  const openModal = (food)=>{
+    setCurrentFood(food);
+    setModalVisible(true);
+  }
   return (
+    <SafeAreaView>
     <ScrollView contentContainerStyle={styles.container}>
       <View>
         <Searchbar
@@ -52,10 +62,45 @@ const FoodDatabase = () => {
                 return null;
               })}
             </View>
+            <Button
+              onPress={() => openModal(hint.food.label)}
+              style={[styles.button, { backgroundColor: '#2196F3' }]}
+              labelStyle={styles.buttonLabel}>
+              Show Modal
+            </Button>
           </View>
         ))}
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            setModalVisible(!modalVisible);
+          }}>
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalText}>Hello World!{currentFood}</Text>
+              <DatePickerInput
+                locale="en-GB"
+                label="Sélectionner une date"
+                value={selectedDate}
+                mode="date"
+                onChange={(event, selectedDate) => {
+                  setSelectedDate(selectedDate);
+                }}
+                style={styles.datePicker}
+              />
+              <Button
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => setModalVisible(!modalVisible)}>
+                <Text style={styles.textStyle}>Hide Modal</Text>
+              </Button>
+            </View>
+          </View>
+        </Modal>
       </View>
     </ScrollView>
+    </SafeAreaView>
   );
 };
 
@@ -85,6 +130,54 @@ const styles = StyleSheet.create({
   },
   measureContainer: {
     marginBottom: 8,
+  },
+  button: {
+    marginTop: 10,
+  },
+  buttonLabel: {
+    color: '#ffffff',
+    fontWeight: 'bold',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
   },
 });
 
